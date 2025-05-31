f949a1d7de8fc65cbb64cdde966c70e1075343b9
SaiKG <saikumar.gurugu.com>
2025-05-31 16:08:45 +1000

    forgot password

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 # users/urls.py
 from django.urls import path
-from users.views import LoginView, FirebaseAuthView, UserManagementView, SignUpView
+from users.views import LoginView, FirebaseAuthView, UserManagementView, SignUpView, ForgotPasswordView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('firebase/', FirebaseAuthView.as_view(), name='firebase_auth'),
     path('user/', UserManagementView.as_view(), name='user_management'),  # Combined view for register and delete
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('forgot-password/', ForgotPasswordView.as_view(), name='forgot-password'),
 ]
