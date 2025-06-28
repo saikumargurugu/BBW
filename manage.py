88e4a223560dd4e52b7e7003894d81c11daadff8
SaiKG <saikumar.gurugu.com>
2025-06-28 15:37:51 +1000

    fix

--- manage.py
+++ manage.py
@@ -6,7 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bbw_backend.settings')
+    from dotenv import load_dotenv
+    load_dotenv()
+    DJANGO_SETTINGS_MODULE = os.environ.get('DJANGO_SETTINGS_MODULE', 'bbw_backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', DJANGO_SETTINGS_MODULE)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
